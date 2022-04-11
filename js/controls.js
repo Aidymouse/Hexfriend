88da0a1d517efcf152d7d028b0e39bcde3b2d869
Aidymouse <aidan@thewizardscat.co.nz>
2022-04-12 00:19:30 +1200

    DELETE EVERYTHING!!!

--- js/controls.js
+++
@@ -1,6 +0,0 @@
-/* The control tips in the bottom right side of the screen */
-
-function controls_showControl(controlName) {
-    $(".controls-listing").addClass("hidden");
-    $("#controls_" + controlName).removeClass("hidden");
-}
