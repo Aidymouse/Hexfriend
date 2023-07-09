9bdb538a3ae0db6588463addb9c0175ea9745b50
Aidymouse <aidan@thewizardscat.co.nz>
2023-07-10 10:57:52 +1200

    save data version update to incorporate gap. Version update

--- src/components/Settings.svelte
+++ src/components/Settings.svelte
@@ -431,23 +431,7 @@
 						renderGrid();
 					}}
 				/>
-				<label for="gridGap">Grid Gap</label>
-				<input
-					id="gap"
-					type="number"
-					min="0"
-					max="99"
-					bind:value={tfield.grid.gap}
-					on:focus={() => {
-						comp_iconLayer.saveOldHexMeasurements(tfield.hexWidth, tfield.hexHeight, tfield.grid.gap);
-					}}
-					on:change={() => {
-						redrawEntireMap();
-						comp_coordsLayer.updateAllCoordPositions();
-						if (retainIconPosition) comp_iconLayer.retainIconPositionOnHexResize(tfield.hexWidth, tfield.hexHeight, tfield.grid.gap);
-						comp_iconLayer.saveOldHexMeasurements(tfield.hexWidth, tfield.hexHeight, tfield.grid.gap);
-					}}
-				/>
+
 				<label for="gridColor">Grid Color</label>
 				<ColorInputPixi
 					bind:value={tfield.grid.stroke}
@@ -457,6 +441,24 @@
 					id={'gridColor'}
 				/>
 			{/if}
+
+			<label for="gridGap">Gap</label>
+			<input
+				id="gap"
+				type="number"
+				min="0"
+				max="99"
+				bind:value={tfield.grid.gap}
+				on:focus={() => {
+					comp_iconLayer.saveOldHexMeasurements(tfield.hexWidth, tfield.hexHeight, tfield.grid.gap);
+				}}
+				on:change={() => {
+					redrawEntireMap();
+					comp_coordsLayer.updateAllCoordPositions();
+					if (retainIconPosition) comp_iconLayer.retainIconPositionOnHexResize(tfield.hexWidth, tfield.hexHeight, tfield.grid.gap);
+					comp_iconLayer.saveOldHexMeasurements(tfield.hexWidth, tfield.hexHeight, tfield.grid.gap);
+				}}
+			/>
 
 			<!-- LARGE HEXES -->
 			<label for="showOverlay">Large Hexes</label>
@@ -1014,6 +1016,12 @@
 		</h2>
 
 		<div id="changelog" class:hidden={hidden_settings.changelog}>
+			<p>Version 1.8.2</p>
+			<ul class="helperText">
+				<li>Added grid gap (thanks Evan!)</li>
+				<li>Save data update to version 8</li>
+			</ul>
+			
 			<p>Version 1.8.1</p>
 			<ul class="helperText">
 				<li>Added changelog</li>
@@ -1030,7 +1038,7 @@
 	<div class="setting-container">
 		<h2>About</h2>
 		<p class="helperText">
-			Hexfriend version 1.8.1 - "New stripes, Hexfriend"
+			Hexfriend version 1.8.2 - "New stripes, Hexfriend"
 		</p>
 		
 		<p class="helperText">
