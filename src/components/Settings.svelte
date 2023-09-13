883193f080ef83866cf3ae327f33501580df24b2
Aidymouse <aidan@thewizardscat.co.nz>
2023-09-14 10:59:29 +1200

    version number update

--- src/components/Settings.svelte
+++ src/components/Settings.svelte
@@ -1145,7 +1145,7 @@
 	<div class="setting-container">
 		<h2>About</h2>
 		<p class="helper-text">
-			Hexfriend version 1.9.5 - "Sorting out your internals, Hexfriend"
+			Hexfriend version 1.9.6 - "Sorting out your internals, Hexfriend"
 		</p>
 		
 		<p class="helper-text">
