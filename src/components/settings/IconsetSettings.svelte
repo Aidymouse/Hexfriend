0d34a2f73a6cc1d776880f7b9491fa8c4af0ef3e
Aidymouse <aidan@thewizardscat.co.nz>
2025-07-29 00:22:31 +1200

    [Settings] tileset settings looks for hfts files now

--- src/components/settings/IconsetSettings.svelte
+++ src/components/settings/IconsetSettings.svelte
@@ -1,112 +1,112 @@
 <script lang="ts">
-        
-    import "../../styles/settings.css";
-    
-    import { LATESTDEFAULTICONSVERSION } from '../../types/savedata';
-    import { type Iconset } from "../../types/icon";
+  import '../../styles/settings.css'
 
-    import { store_has_unsaved_changes } from "../../stores/flags";
-    import { tl } from "../../stores/translation";
-    
-    import * as texture_loader from '../../lib/texture_loader';
+  import { LATESTDEFAULTICONSVERSION } from '../../types/savedata'
+  import { type Iconset } from '../../types/icon'
 
-    export let loadedSave;
-    export let loadedIconsets;
-    export let iconset_text;
+  import { store_has_unsaved_changes } from '../../stores/flags'
+  import { tl } from '../../stores/translation'
 
-    export let appState;
+  import * as texture_loader from '../../lib/texture_loader'
 
-    function removeIconset(setId: string) {
-		loadedIconsets = loadedIconsets.filter((is: Iconset) => is.id != setId);
-		loadedSave.iconsets = loadedIconsets;
+  export let loadedSave
+  export let loadedIconsets
+  export let iconset_text
 
-		$store_has_unsaved_changes = true;
-	}
+  export let appState
 
-	let iconsetFiles: FileList;
+  function removeIconset(setId: string) {
+    loadedIconsets = loadedIconsets.filter((is: Iconset) => is.id != setId)
+    loadedSave.iconsets = loadedIconsets
 
-	function importIconset() {
-		let importFile = iconsetFiles[0];
+    $store_has_unsaved_changes = true
+  }
 
-		if (!importFile) return;
+  let iconsetFiles: FileList
 
-		let r = new FileReader();
-		r.readAsText(importFile);
-		r.onload = (eb) => {
-		    /* Read the file */
-		    let setToImport = JSON.parse(eb.target.result as string);
+  function importIconset() {
+    let importFile = iconsetFiles[0]
 
-		    /* Check that set hasn't already been imported */
-		    if (loadedIconsets.find((is: Iconset) => is.id == setToImport.id) != null) {
-			alert($tl.settings.icon_sets.already_loaded);
-			return;
-		    }
+    if (!importFile) return
 
-		    loadedIconsets.push(setToImport);
-		    loadedIconsets = loadedIconsets;
+    let r = new FileReader()
+    r.readAsText(importFile)
+    r.onload = (eb) => {
+      /* Read the file */
+      let setToImport = JSON.parse(eb.target.result as string)
 
-		    /* We also have to load all of these textures */
-		    //addIconsetTextures(setToImport, L);
-		    texture_loader.load_iconset_textures(setToImport);
+      /* Check that set hasn't already been imported */
+      if (loadedIconsets.find((is: Iconset) => is.id == setToImport.id) != null) {
+        alert($tl.settings.icon_sets.already_loaded)
+        return
+      }
 
-		    $store_has_unsaved_changes = true;
-		};
-	}
+      loadedIconsets.push(setToImport)
+      loadedIconsets = loadedIconsets
 
+      /* We also have to load all of these textures */
+      //addIconsetTextures(setToImport, L);
+      texture_loader.load_iconset_textures(setToImport)
+
+      $store_has_unsaved_changes = true
+    }
+  }
 </script>
 
-
 <div id="iconsets">
-    {#each loadedIconsets as iconset (iconset.id)}
-        <div class="loaded-tileset">{iconset.name}
+  {#each loadedIconsets as iconset (iconset.id)}
+    <div class="loaded-tileset">
+      {iconset.name}
 
-            {#if iconset.id.split(":")[0] != 'default' && iconset.version != LATESTDEFAULTICONSVERSION}
-                <button on:click={() => { removeIconset(iconset.id); }}>
-                    <img src="/assets/img/tools/trash.png" alt={'Trash'} title={'Remove Iconset'} />
-                </button>
-            {/if}
-        </div>
-    {/each}
-
-    <span>
-	<button class="file-input-button">{$tl.settings.icon_sets.import}
-            <input
-                type="file"
-                accept=".hfis"
-                bind:files={iconsetFiles}
-                on:change={() => {
-                    importIconset();
-                }}
-            /></button
-        >
+      {#if iconset.id.split(':')[0] != 'default' && iconset.version != LATESTDEFAULTICONSVERSION}
         <button
-            on:click={() => {
-                appState = 'iconsetCreator';
-	    }}>{$tl.settings.icon_sets.builder}</button
+          on:click={() => {
+            removeIconset(iconset.id)
+          }}
         >
-    </span>
+          <img src="/assets/img/tools/trash.png" alt={'Trash'} title={'Remove Iconset'} />
+        </button>
+      {/if}
+    </div>
+  {/each}
+
+  <span>
+    <button class="file-input-button"
+      >{$tl.settings.icon_sets.import}
+      <input
+        type="file"
+        accept=".hfis"
+        bind:files={iconsetFiles}
+        on:change={() => {
+          importIconset()
+        }}
+      /></button
+    >
+    <button
+      on:click={() => {
+        appState = 'iconsetCreator'
+      }}>{$tl.settings.icon_sets.builder}</button
+    >
+  </span>
 </div>
 
 <style>
-    button {
-	min-height: 30px;
-    }
+  button {
+    min-height: 30px;
+  }
 
-    #iconsets {
-	display: grid;
-	grid-template-columns: 1fr;
-	grid-template-rows: 30px;
-	grid-auto-rows: auto;
-	row-gap: 5px;
-	margin-top: var(--large-radius);
-    }
+  #iconsets {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 30px;
+    grid-auto-rows: auto;
+    row-gap: 5px;
+    margin-top: var(--large-radius);
+  }
 
-    
-
-    #iconsets span {
-	display: grid;
-	grid-template-columns: 1fr 1fr;
-	column-gap: 5px;
-    }
-
+  #iconsets span {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    column-gap: 5px;
+  }
 </style>
--- src/components/settings/TilesetSettings.svelte
+++ src/components/settings/TilesetSettings.svelte
@@ -1,199 +1,182 @@
 <script lang="ts">
-	import "../../styles/settings.css";
+  import '../../styles/settings.css'
 
-	import type { Tileset } from "../../types/tilesets";
+  import type { Tileset } from '../../types/tilesets'
 
-	import {
-		LATESTTILESETFORMATVERSION,
-		LATESTDEFAULTTILESETVERSION,
-	} from "../../types/tilesets";
+  import { LATESTTILESETFORMATVERSION, LATESTDEFAULTTILESETVERSION } from '../../types/tilesets'
 
-	import { store_has_unsaved_changes } from "../../stores/flags";
-	import { tfield } from "../../stores/tfield";
-	import { tl } from "../../stores/translation";
+  import { store_has_unsaved_changes } from '../../stores/flags'
+  import { tfield } from '../../stores/tfield'
+  import { tl } from '../../stores/translation'
 
-	import {
-		update_map_to_new_default_tileset,
-		update_tileset_format,
-	} from "../../lib/tileset_updater";
-	import * as texture_loader from "../../lib/texture_loader";
+  import { update_map_to_new_default_tileset, update_tileset_format } from '../../lib/tileset_updater'
+  import * as texture_loader from '../../lib/texture_loader'
 
-	import { get_tileset_id } from "../../helpers/tiles";
-	import { DEFAULTTILESET } from "../../lib/defaultTileset";
+  import { get_tileset_id } from '../../helpers/tiles'
+  import { DEFAULTTILESET } from '../../lib/defaultTileset'
 
-	export let comp_terrainLayer;
-	export let comp_terrain_panel;
+  export let comp_terrainLayer
+  export let comp_terrain_panel
 
-	export let loadedSave;
-	export let loadedTilesets: Tileset[];
+  export let loadedSave
+  export let loadedTilesets: Tileset[]
 
-	export let appState;
+  export let appState
 
-	let tilesetFiles: FileList;
+  let tilesetFiles: FileList
 
-	function importTileset() {
-		let importFile = tilesetFiles[0];
+  function importTileset() {
+    let importFile = tilesetFiles[0]
 
-		if (!importFile) return;
+    if (!importFile) return
 
-		let r = new FileReader();
-		r.readAsText(importFile);
-		r.onload = (eb) => {
-			/* Read the file */
-			let setToImport = JSON.parse(eb.target.result as string);
+    let r = new FileReader()
+    r.readAsText(importFile)
+    r.onload = (eb) => {
+      /* Read the file */
+      let setToImport = JSON.parse(eb.target.result as string)
 
-			let set_already_imported = loadedTilesets.find(
-				(ts: Tileset) => ts.id == setToImport.id,
-			);
+      let set_already_imported = loadedTilesets.find((ts: Tileset) => ts.id == setToImport.id)
 
-			/* Check that set hasn't already been imported */
-			if (set_already_imported != null) {
-				alert($tl.settings.tilesets.already_loaded);
-				return;
-			}
+      /* Check that set hasn't already been imported */
+      if (set_already_imported != null) {
+        alert($tl.settings.tilesets.already_loaded)
+        return
+      }
 
-			if (setToImport.format_version < LATESTTILESETFORMATVERSION) {
-				setToImport = update_tileset_format(setToImport);
-			}
+      if (setToImport.format_version < LATESTTILESETFORMATVERSION) {
+        setToImport = update_tileset_format(setToImport)
+      }
 
-			loadedTilesets.push(setToImport);
-			loadedTilesets = loadedTilesets;
+      loadedTilesets.push(setToImport)
+      loadedTilesets = loadedTilesets
 
-			/* We also have to load all of these textures */
-			//addTilesetTextures(setToImport, L);
-			texture_loader.load_tileset_textures(setToImport);
-			$store_has_unsaved_changes = true;
-		};
-	}
+      /* We also have to load all of these textures */
+      //addTilesetTextures(setToImport, L);
+      texture_loader.load_tileset_textures(setToImport)
+      $store_has_unsaved_changes = true
+    }
+  }
 
-	function removeTileset(setId: string) {
-		if (!confirm($tl.settings.tilesets.remove_confirmation)) return;
+  function removeTileset(setId: string) {
+    if (!confirm($tl.settings.tilesets.remove_confirmation)) return
 
-		comp_terrainLayer.removeAllTilesOfSet(setId);
-		comp_terrain_panel.reset_tile();
+    comp_terrainLayer.removeAllTilesOfSet(setId)
+    comp_terrain_panel.reset_tile()
 
-		// This line will need to change if the default tileset ever gets removeable
-		//data_terrain.tile = {...loadedTilesets[0].tiles[0]}
+    // This line will need to change if the default tileset ever gets removeable
+    //data_terrain.tile = {...loadedTilesets[0].tiles[0]}
 
-		loadedTilesets = loadedTilesets.filter((ts: Tileset) => ts.id != setId);
-		loadedSave.tilesets = loadedTilesets;
+    loadedTilesets = loadedTilesets.filter((ts: Tileset) => ts.id != setId)
+    loadedSave.tilesets = loadedTilesets
 
-		$store_has_unsaved_changes = true;
+    $store_has_unsaved_changes = true
 
-		// Maybe we should remove tiles here, because otherwise the tiles just... fail to load.
-		// Check if these tiles are being used anywere
-	}
+    // Maybe we should remove tiles here, because otherwise the tiles just... fail to load.
+    // Check if these tiles are being used anywere
+  }
 
-	function update_default_tileset() {
-		let successfully_updated = update_map_to_new_default_tileset($tfield);
-		if (!successfully_updated) return;
+  function update_default_tileset() {
+    let successfully_updated = update_map_to_new_default_tileset($tfield)
+    if (!successfully_updated) return
 
-		// Remove default tileset
-		loadedTilesets = loadedTilesets.filter(
-			(ts) => get_tileset_id(ts) != "default",
-		);
-		loadedTilesets.push(DEFAULTTILESET);
-		loadedSave.tilesets = loadedTilesets;
-	}
+    // Remove default tileset
+    loadedTilesets = loadedTilesets.filter((ts) => get_tileset_id(ts) != 'default')
+    loadedTilesets.push(DEFAULTTILESET)
+    loadedSave.tilesets = loadedTilesets
+  }
 </script>
 
 <div id="tilesets">
-	{#each loadedTilesets as tileset (tileset.id)}
-		<div
-			class="loaded-tileset"
-			on:click={() => {
-				console.log(tileset);
-			}}
-			on:keydown={() => {}}
-		>
-			<span style="display: flex;"
-				>{tileset.name}
-				<span class="helper-text">v{tileset.version}</span></span
-			>
+  {#each loadedTilesets as tileset (tileset.id)}
+    <div
+      class="loaded-tileset"
+      on:click={() => {
+        console.log(tileset)
+      }}
+      on:keydown={() => {}}
+    >
+      <span style="display: flex;"
+        >{tileset.name}
+        <span class="helper-text">v{tileset.version}</span></span
+      >
 
-			{#if get_tileset_id(tileset) != "default"}
-				<button
-					class="set-rollover-button"
-					on:click={() => {
-						removeTileset(tileset.id);
-					}}
-				>
-					<img
-						src="/assets/img/tools/trash.png"
-						alt={"Trash"}
-						title={"Remove Tileset"}
-					/>
-				</button>
-			{/if}
+      {#if get_tileset_id(tileset) != 'default'}
+        <button
+          class="set-rollover-button"
+          on:click={() => {
+            removeTileset(tileset.id)
+          }}
+        >
+          <img src="/assets/img/tools/trash.png" alt={'Trash'} title={'Remove Tileset'} />
+        </button>
+      {/if}
 
-			<!-- Update Default Tileset Button -->
-			{#if get_tileset_id(tileset) == "default" && tileset.version < LATESTDEFAULTTILESETVERSION}
-				<button
-					id="default-tileset-update-button"
-					on:click={() => {
-						update_default_tileset();
-					}}
-					title={`Update Tileset to v${LATESTDEFAULTTILESETVERSION}`}
-				>
-					<img
-						src="/assets/img/ui/arrow.png"
-						alt={""}
-						title={`Update Tileset to v${LATESTDEFAULTTILESETVERSION}`}
-					/>
-				</button>
-			{/if}
-		</div>
-	{/each}
+      <!-- Update Default Tileset Button -->
+      {#if get_tileset_id(tileset) == 'default' && tileset.version < LATESTDEFAULTTILESETVERSION}
+        <button
+          id="default-tileset-update-button"
+          on:click={() => {
+            update_default_tileset()
+          }}
+          title={`Update Tileset to v${LATESTDEFAULTTILESETVERSION}`}
+        >
+          <img src="/assets/img/ui/arrow.png" alt={''} title={`Update Tileset to v${LATESTDEFAULTTILESETVERSION}`} />
+        </button>
+      {/if}
+    </div>
+  {/each}
 
-	<span>
-		<button class="file-input-button"
-			>{$tl.settings.tilesets.import}<input
-				type="file"
-				bind:files={tilesetFiles}
-				on:change={() => {
-					importTileset();
-				}}
-			/></button
-		>
-		<button
-			on:click={() => {
-				appState = "tilesetCreator";
-			}}>{$tl.settings.tilesets.builder}</button
-		>
-	</span>
+  <span>
+    <button class="file-input-button"
+      >{$tl.settings.tilesets.import}<input
+        type="file"
+        accept=".hfts"
+        bind:files={tilesetFiles}
+        on:change={() => {
+          importTileset()
+        }}
+      /></button
+    >
+    <button
+      on:click={() => {
+        appState = 'tilesetCreator'
+      }}>{$tl.settings.tilesets.builder}</button
+    >
+  </span>
 </div>
 
 <style>
-	button {
-		min-height: 30px;
-	}
+  button {
+    min-height: 30px;
+  }
 
-	#tilesets {
-		display: grid;
-		grid-template-columns: 1fr;
-		grid-template-rows: 30px;
-		grid-auto-rows: auto;
-		row-gap: 5px;
-		margin-top: var(--large-radius);
-	}
+  #tilesets {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 30px;
+    grid-auto-rows: auto;
+    row-gap: 5px;
+    margin-top: var(--large-radius);
+  }
 
-	#default-tileset-update-button {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-	}
+  #default-tileset-update-button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 
-	.loaded-tileset:hover button.set-rollover-button {
-		opacity: 1;
-	}
+  .loaded-tileset:hover button.set-rollover-button {
+    opacity: 1;
+  }
 
-	.loaded-tileset button.set-rollover-button {
-		opacity: 0;
-	}
+  .loaded-tileset button.set-rollover-button {
+    opacity: 0;
+  }
 
-	#tilesets span {
-		display: grid;
-		grid-template-columns: 1fr 1fr;
-		column-gap: 5px;
-	}
+  #tilesets span {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    column-gap: 5px;
+  }
 </style>
