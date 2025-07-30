5bfba4eff5cd56bc769300a3da8f69164d5e6cea
Aidymouse <aidan@thewizardscat.co.nz>
2025-07-31 00:20:03 +1200

    [Icon] add scale mode, icons now have two variants

--- src/types/icon.ts
+++ src/types/icon.ts
@@ -1,18 +1,35 @@
+import type { ScaleMode } from "../helpers/imageSizing";
+
 /* SAVE DATA */
-export interface Icon {
+export type Icon = {
 	display: string;
 	texId: string;
 	id: string;
 	color: number;
-	pHex: number; // percent of total hex taken up, where 1 = 100% of hexes shortest dimension
+	//pHex: number; 
 	base64: string;
-	preview: string;
+	preview: string; // Preview is set to nothing on the icon layer itself
 	texWidth: number;
 	texHeight: number;
 	rotation: number;
-}
+    scaleMode: ScaleMode // Needed?
+    // scale: {
+    //     percent_of_hex: number
 
-export interface Iconset {
+    // } |  'percent_of_hex' | 'seperate_scales'
+} & ({
+    scaleMode: ScaleMode.RELATIVE,
+    pHex: number // percent of total hex taken up, where 1 = 100% of hexes shortest dimension
+} | {
+    scaleMode: ScaleMode.BYDIMENSION,
+    pWidth: number,
+    pHeight: number
+
+})
+
+export type IconLayerIcon = Exclude<Icon, 'id'> & {id: number}
+
+export type Iconset = {
 	name: string;
 	id: string;
 	author: string;
@@ -22,13 +39,13 @@ export interface Iconset {
 }
 
 /* Used during program and also saved in list to keep track of icons */
-export interface IconLayerIcon {
-	x: number;
-	y: number;
-	color: number;
-	scale: number;
-	pHex: number;
-	id: number;
-	texId: string;
-	rotation: number;
-}
+// export interface IconLayerIcon {
+// 	x: number;
+// 	y: number;
+// 	color: number;
+// 	scale: number;
+// 	pHex: number;
+// 	id: number;
+// 	texId: string;
+// 	rotation: number;
+// }
