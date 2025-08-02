45a5efa602030c8e9514493c91782f0c2f8a8e52
Aidan <aidymouse4891@gmail.com>
2025-08-02 14:25:29 +1200

    [Alert] add alert component

---
+++ src/components/Alert.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  export let severity: 'warn' | 'error';
+</script>
+
+<p class={`alert ${severity}`}>
+  <slot />
+</p>
+
+<style>
+  p.alert {
+    padding: 0.5em;
+    border-radius: var(--small-radius);
+    font-size: 11pt;
+  }
+
+  p.warn {
+    background-color: #ffdb4d;
+    color: #804d00;
+  }
+
+</style>
