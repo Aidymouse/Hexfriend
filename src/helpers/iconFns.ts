1c5791f07937e753a49db8cf30697583837dad10
aidan.habedank@iotventures.net <aidan.habedank@iotventures.net>
2025-08-05 01:14:39 +1200

    [Prettier] iconfns and imagesizing

--- src/helpers/iconFns.ts
+++ src/helpers/iconFns.ts
@@ -7,82 +7,81 @@ import { get_icon_scale_for_hex, get_image_scaled_for_hex_relative, ScaleMode } 
 
 
 export type PreviewHexInfo = {
-    hexWidth: number,
-    hexHeight: number,
-    orientation: HexOrientation,
-    color: string
+	hexWidth: number,
+	hexHeight: number,
+	orientation: HexOrientation,
+	color: string
 }
 
 /** Generates an icon preview by drawing a specified hex, then an icon on top, turning it into base 64, and returning the base 64. */
 export async function generate_icon_preview(icon: Icon, hexInfo: PreviewHexInfo, grph: PIXI.Graphics, spr: PIXI.Sprite, cont: PIXI.Container, app: PIXI.Application, customTexture?: PIXI.Texture): Promise<string> {
 
-		let path = getHexPath(hexInfo.hexWidth, hexInfo.hexHeight, hexInfo.orientation, 0, 0);
-		grph.clear();
-		grph.beginFill(hexInfo.color);
-		grph.drawPolygon(path);
-		grph.endFill();
+	let path = getHexPath(hexInfo.hexWidth, hexInfo.hexHeight, hexInfo.orientation, 0, 0);
+	grph.clear();
+	grph.beginFill(hexInfo.color);
+	grph.drawPolygon(path);
+	grph.endFill();
 
-		const icon_scale = get_icon_scale_for_hex(icon, hexInfo)
-		const matrix = new PIXI.Matrix().rotate(PIXI.DEG_TO_RAD * icon.rotation).scale(icon_scale.x, icon_scale.y);
-
-
-		spr.texture = customTexture ?? get_icon_texture(icon.texId);
-		spr.tint = icon.color;
-		spr.anchor.set(0.5, 0.5);
-		// spr.rotation = PIXI.DEG_TO_RAD * (icon.rotation ?? 0);
-		// spr.scale.x = icon_scale.x
-		// spr.scale.y = icon_scale.y
-		spr.transform.setFromMatrix(matrix);
-
-		let b64 = await app.renderer.extract.base64(cont); //PIXI.autoDetectRenderer().plugins.extract.base64(c)
-
-		return b64;
-	}
+	const icon_scale = get_icon_scale_for_hex(icon, hexInfo)
+	const matrix = new PIXI.Matrix().rotate(PIXI.DEG_TO_RAD * icon.rotation).scale(icon_scale.x, icon_scale.y);
 
 
-	/** This works!!! but it sucks. Can I make it not? */
+	spr.texture = customTexture ?? get_icon_texture(icon.texId);
+	spr.tint = icon.color;
+	spr.anchor.set(0.5, 0.5);
+	// spr.rotation = PIXI.DEG_TO_RAD * (icon.rotation ?? 0);
+	// spr.scale.x = icon_scale.x
+	// spr.scale.y = icon_scale.y
+	spr.transform.setFromMatrix(matrix);
+
+	let b64 = await app.renderer.extract.base64(cont); //PIXI.autoDetectRenderer().plugins.extract.base64(c)
+
+	return b64;
+}
+
+
 export async function generate_ordered_icon_preview(icon: Icon, hexInfo: PreviewHexInfo, customTexture: PIXI.Texture): Promise<string> {
 
-		// Sprite rotates, container scales
-		const app = new PIXI.Application();
-		const spr = new PIXI.Sprite();
-		const grph = new PIXI.Graphics();
-		const cont = new PIXI.Container();
-		// const cont_scaler = new PIXI.Container();
-		cont.addChild(grph);
-		cont.addChild(spr);
-		// cont.addChild(cont_scaler);
-		// cont_scaler.addChild(spr);
+	// Sprite rotates, container scales
+	const app = new PIXI.Application();
+	const spr = new PIXI.Sprite();
+	const grph = new PIXI.Graphics();
+	const cont = new PIXI.Container();
+	// const cont_scaler = new PIXI.Container();
+	cont.addChild(grph);
+	cont.addChild(spr);
+	// cont.addChild(cont_scaler);
+	// cont_scaler.addChild(spr);
 
-		let path = getHexPath(hexInfo.hexWidth, hexInfo.hexHeight, hexInfo.orientation, 0, 0);
-		grph.clear();
-		grph.beginFill(hexInfo.color);
-		grph.drawPolygon(path);
-		grph.endFill();
+	let path = getHexPath(hexInfo.hexWidth, hexInfo.hexHeight, hexInfo.orientation, 0, 0);
+	grph.clear();
+	grph.beginFill(hexInfo.color);
+	grph.drawPolygon(path);
+	grph.endFill();
 
-		const icon_scale = get_icon_scale_for_hex(icon, hexInfo)
-		
-		const mtrx_spr = new PIXI.Matrix();
-		mtrx_spr.rotate(PIXI.DEG_TO_RAD * icon.rotation);
-		mtrx_spr.scale(icon_scale.x, icon_scale.y);
+	const icon_scale = get_icon_scale_for_hex(icon, hexInfo)
+
+	const mtrx_spr = new PIXI.Matrix();
+	mtrx_spr.rotate(PIXI.DEG_TO_RAD * icon.rotation);
+	mtrx_spr.scale(icon_scale.x, icon_scale.y);
 
 
 
-		spr.texture = customTexture ?? get_icon_texture(icon.texId);
-		spr.tint = icon.color;
-		spr.anchor.set(0.5, 0.5);
-		spr.transform.setFromMatrix(mtrx_spr)
-		// spr.rotation = PIXI.DEG_TO_RAD * (icon.rotation ?? 0);
-		// cont_scaler.scale.x = icon_scale.x
-		// cont_scaler.scale.y = icon_scale.y
+	spr.texture = customTexture ?? get_icon_texture(icon.texId);
+	spr.tint = icon.color;
+	spr.anchor.set(0.5, 0.5);
+	spr.transform.setFromMatrix(mtrx_spr)
+	// spr.rotation = PIXI.DEG_TO_RAD * (icon.rotation ?? 0);
+	// cont_scaler.scale.x = icon_scale.x
+	// cont_scaler.scale.y = icon_scale.y
 
-		let b64 = await app.renderer.extract.base64(cont); //PIXI.autoDetectRenderer().plugins.extract.base64(c)
+	let b64 = await app.renderer.extract.base64(cont); //PIXI.autoDetectRenderer().plugins.extract.base64(c)
 
-		spr.destroy();
-		grph.destroy();
-		// cont_scaler.destroy();
-		cont.destroy();
-		app.destroy();
+	spr.destroy();
+	grph.destroy();
+	// cont_scaler.destroy();
+	cont.destroy();
+	app.destroy();
 
-		return b64;
+	return b64;
 }
--- src/helpers/imageSizing.ts
+++ src/helpers/imageSizing.ts
@@ -3,8 +3,8 @@ import { type PreviewHexInfo } from './iconFns'
 
 /** I better make sure I like these names because they can NEVER CHANGE! Or old save versions will need to know what they used to be */
 export enum ScaleMode {
-    RELATIVE = 'relative',
-    BYDIMENSION = 'bydimension',
+	RELATIVE = 'relative',
+	BYDIMENSION = 'bydimension',
 }
 /** Takes an object of width/height and returns a scale such that when applied to width/height they become scaled relative to hex size.
  * The proportion is always of the hexes smaller dimension, and scale is found with corresonding image dimension, then applied to the other dimension as well
@@ -18,35 +18,35 @@ export enum ScaleMode {
  */
 export const get_image_scaled_for_hex_relative = (texWidth: number, texHeight: number, hexWidth: number, hexHeight: number, proportion_of_hex: number) => {
 
-    let scale: number;
-    if (hexWidth < hexHeight) {
-      scale = (hexWidth * proportion_of_hex) / 100 / texWidth
-    } else {
-      scale = (hexHeight * proportion_of_hex) / 100 / texHeight
-    }
+	let scale: number;
+	if (hexWidth < hexHeight) {
+		scale = (hexWidth * proportion_of_hex) / 100 / texWidth
+	} else {
+		scale = (hexHeight * proportion_of_hex) / 100 / texHeight
+	}
 
-    return { x: scale, y: scale }
+	return { x: scale, y: scale }
 }
 
 /** Takes an object of width/height and returns a scale such that when applied to width/height they become scaled relative to hex size 
  * Scales such that the dimension of the image will always be that percentage of the hexes dimension
 */
 export const get_image_scaled_for_hex_bydimension = (texWidth: number, texHeight: number, hexWidth: number, hexHeight: number, proportion_of_hex_width: number, proportion_of_hex_height: number) => {
-    return { 
-        x: (hexWidth * proportion_of_hex_width) / 100 / texWidth,
-        y: (hexHeight * proportion_of_hex_height) / 100 / texHeight 
-    }
+	return {
+		x: (hexWidth * proportion_of_hex_width) / 100 / texWidth,
+		y: (hexHeight * proportion_of_hex_height) / 100 / texHeight
+	}
 }
 
 /** Wraps the image scaling functions but takes in an icon. Defaults to relative scale mode if none is present */
 export const get_icon_scale_for_hex = (icon: Icon, hexInfo: Pick<PreviewHexInfo, 'hexWidth' | 'hexHeight'>) => {
-  if (icon.scaleMode === ScaleMode.BYDIMENSION) {
-    return get_image_scaled_for_hex_bydimension(icon.texWidth, icon.texHeight, hexInfo.hexWidth, hexInfo.hexHeight, icon.pWidth, icon.pHeight)
-  }
+	if (icon.scaleMode === ScaleMode.BYDIMENSION) {
+		return get_image_scaled_for_hex_bydimension(icon.texWidth, icon.texHeight, hexInfo.hexWidth, hexInfo.hexHeight, icon.pWidth, icon.pHeight)
+	}
 
-  if (!icon.scaleMode) {
-  	console.warn("get_icon_scale_for_hex", "Icon does not have a valid scale mode:", icon.scaleMode)
-  }
-  return get_image_scaled_for_hex_relative(icon.texWidth, icon.texHeight, hexInfo.hexWidth, hexInfo.hexHeight, icon.pHex)
-  
+	if (!icon.scaleMode) {
+		console.warn("get_icon_scale_for_hex", "Icon does not have a valid scale mode:", icon.scaleMode)
+	}
+	return get_image_scaled_for_hex_relative(icon.texWidth, icon.texHeight, hexInfo.hexWidth, hexInfo.hexHeight, icon.pHex)
+
 }
