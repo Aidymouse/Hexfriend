b929204be2d71b59ef439ede2d98262c10be496b
Aidan <aidymouse4891@gmail.com>
2025-08-28 21:28:30 +1200

    [UndoHandler] handle case when you undo some stuff then perform more actions + pipe some dimension changing events through

--- src/lib/undo_handler.ts
+++ src/lib/undo_handler.ts
@@ -8,6 +8,9 @@ import { store_undo } from "../stores/undo"
 import type { ComponentType } from "svelte"
 import { store } from "../stores/panning";
 
+import TerrainLayer from '../layers/TerrainLayer.svelte'
+import Settings from '../components/Settings.svelte'
+
 /*
 == General
 Resizing Hexes
@@ -30,6 +33,16 @@ store_undo.subscribe(u => local_undo = u)
 
 
 export const record_undo_action = (action: UndoAction) => {
+
+	if (local_undo.undo_pointer !== local_undo.stack.length) {
+		console.log("Recorded new action not in present")
+		store_undo.update((u) => {
+			u.stack = u.stack.slice(0, u.undo_pointer)
+			console.log(u.stack)
+			//u.undo_pointer += 1;
+			return u
+		})
+	}
 
 	store_undo.update((u) => {
 		u.stack.push(action)
@@ -65,14 +78,24 @@ export const push_undo_action = (): UndoAction | null => {
 }
 
 
+export type UndoComponents = {
+	terrainLayer: TerrainLayer
+	settings: Settings
+}
 
 /** Handles actually undoing */
-export const handle_undo_action = (action: UndoAction, comp_terrainLayer: ComponentType) => {
-	console.log("UNDO", action)
+export const handle_undo_action = (action: UndoAction, components: UndoComponents) => {
+	//console.log("UNDO", action)
 	switch (action.type) {
-		case UndoActions.ChangeHexDimensions: {
+		/** Settings */
+		case UndoActions.ChangeHexDimensions: 
+		case UndoActions.ChangeHexOrientation: {
+			// console.log(components)
+			components.settings.handle_undo(action)
 			break;
 		}
+		/** Terrain */
+		/** Fall back */
 		default: {
 			console.warn("Unhandled undo action", action.type)
 			break;
@@ -80,12 +103,16 @@ export const handle_undo_action = (action: UndoAction, comp_terrainLayer: Compon
 	}
 }
 
-export const handle_redo_action = (action: UndoAction, comp_terrainLayer: ComponentType) => {
+export const handle_redo_action = (action: UndoAction, components: UndoComponents) => {
 	console.log("REDO", action)
 	switch (action.type) {
-		case UndoActions.ChangeHexDimensions: {
+		/** Settings */
+		case UndoActions.ChangeHexDimensions: 
+		case UndoActions.ChangeHexOrientation: {
+			components.settings.handle_redo(action)
 			break;
 		}
+		/** Fallback */
 		default: {
 			console.warn("Unhandled undo action", action.type)
 			break;
