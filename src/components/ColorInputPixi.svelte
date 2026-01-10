a49a3bd2055a9bc90397518d0dd1a5a0bbce2c66
Aidan <aidymouse4891@gmail.com>
2026-01-11 01:36:15 +1300

    Dispatch details on change color input pixi

--- src/components/ColorInputPixi.svelte
+++ src/components/ColorInputPixi.svelte
@@ -1,69 +1,69 @@
 <script lang="ts">
-	import * as PIXI from 'pixi.js';
-	import { createEventDispatcher } from 'svelte';
+  import * as PIXI from 'pixi.js'
+  import { createEventDispatcher } from 'svelte'
 
-	let dispatch = createEventDispatcher();
+  let dispatch = createEventDispatcher()
 
-	export let value: number = 0x000000;
+  export let value: number = 0x000000
 
-	export let id: string = null;
+  export let id: string = null
 
-	export let w = "2em";
-	export let h = "2em";
+  export let w = '2em'
+  export let h = '2em'
 
-	let oldStringValue = PIXI.Color.shared.setValue(value).toHex();
-	let stringValue = oldStringValue;
+  let oldStringValue = PIXI.Color.shared.setValue(value).toHex()
+  let stringValue = oldStringValue
 
-	$: {
-		if (stringValue != oldStringValue) {
-			/* If the old and new values dont match, then the value was changed on the input, not externally */
-			value = PIXI.Color.shared.setValue(stringValue).toNumber();
-			dispatch('input', {string: stringValue, number: value});
-		} else {
-			stringValue = PIXI.Color.shared.setValue(value).toHex();
-		}
-		oldStringValue = stringValue;
+  $: {
+    if (stringValue != oldStringValue) {
+      /* If the old and new values dont match, then the value was changed on the input, not externally */
+      value = PIXI.Color.shared.setValue(stringValue).toNumber()
+      dispatch('input', { string: stringValue, number: value })
+    } else {
+      stringValue = PIXI.Color.shared.setValue(value).toHex()
+    }
+    oldStringValue = stringValue
 
-		dispatch('change', {});
-	}
+    dispatch('change', { string: stringValue, number: value })
+  }
 </script>
 
 <span style="width: {w}; height: {h}">
-	<div style="--user-input-color: {stringValue};">
-		<input type="color" bind:value={stringValue} {id} />
-	</div>
+  <div style="--user-input-color: {stringValue};">
+    <input type="color" bind:value={stringValue} {id} />
+  </div>
 </span>
 
 <style>
-	span {
-		display: flex;
-		gap: 0.5em;
-		align-items: center;
-		justify-content: center;
-	}
+  span {
+    display: flex;
+    gap: 0.5em;
+    align-items: center;
+    justify-content: center;
+  }
 
-	div {
-		border-radius: 50%;
-		overflow: none;
-		border: solid 0.125em var(--text);
-		box-sizing: border-box;
-		background-color: var(--user-input-color);
+  div {
+    border-radius: 50%;
+    overflow: none;
+    border: solid 0.125em var(--text);
+    box-sizing: border-box;
+    background-color: var(--user-input-color);
 
-		width: 90%;
-		height: 90%;
-		transition-duration: 0.2s;
-		transition-property: border-color;
-	}
+    width: 90%;
+    height: 90%;
+    transition-duration: 0.2s;
+    transition-property: border-color;
+  }
 
-	div:hover {
-		border-color: var(--hexfriend-green);
-		transition-duration: 0.2s;
-		transition-property: border-color;
-	}
+  div:hover {
+    border-color: var(--hexfriend-green);
+    transition-duration: 0.2s;
+    transition-property: border-color;
+  }
 
-	input {
-		width: 100%;
-		height: 100%;
-		opacity: 0;
-	}
+  input {
+    width: 100%;
+    height: 100%;
+    opacity: 0;
+  }
 </style>
