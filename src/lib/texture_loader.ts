4964f6aa7e987957508d4c5b18cc771847bc12ca
Aidan <aidymouse4891@gmail.com>
2026-01-17 15:48:37 +1300

    Remove commented lines from TextureLoader

--- src/lib/texture_loader.ts
+++ src/lib/texture_loader.ts
@@ -10,23 +10,8 @@ export let icon_texture_lookup_table: { [key: string]: string } = {}
 
 export async function load_icon_texture(icon_id: string, base64: string) {
 	let new_texture = await PIXI.Assets.load(base64);
-	console.log(`Loading Icon Texture with id: ${icon_id}`)
 
 	loaded_icon_textures[icon_id] = new_texture
-	
-	/*
-	if (PIXI.Assets.cache.has(icon_id)) {
-		new_texture = PIXI.Cache.get(base64);
-		icon_texture_lookup_table[icon_id] = new_texture.textureCacheIds[1]
-
-	} else {
-		PIXI.Assets.add(icon_id, base64)
-		loaded_icon_textures[icon_id] = await PIXI.Assets.load(icon_id)
-		new_texture = loaded_icon_textures[icon_id]
-		icon_texture_lookup_table[icon_id] = icon_id
-
-	}
-	*/
 
 	return new_texture
 }
