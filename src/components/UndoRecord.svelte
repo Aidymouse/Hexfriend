53abcac938c713dc2fd1f91f314ddfc0f91ed752
Aidan <aidymouse4891@gmail.com>
2026-01-17 15:48:30 +1300

    Spacing on UndoRecord

--- src/components/UndoRecord.svelte
+++ src/components/UndoRecord.svelte
@@ -1,22 +1,22 @@
 <script lang="ts">
-	import {} from '../lib/undo_handler.ts'
-	import { store_undo } from '../stores/undo.ts'
-	import { UndoActions } from '../types/undoTypes.ts'
+  import {} from '../lib/undo_handler.ts'
+  import { store_undo } from '../stores/undo.ts'
+  import { UndoActions } from '../types/undoTypes.ts'
 
-	const format_undo_action = (action: UndoActions): string => {
-		switch (action.type) {
-			case UndoActions.ToggleGridLargeHexes: {
-				return `${action.enabled}`
-			}
-		}
-	}
+  const format_undo_action = (action: UndoActions): string => {
+    switch (action.type) {
+      case UndoActions.ToggleGridLargeHexes: {
+	return `${action.enabled}`
+      }
+    }
+  }
 </script>
 
 <div class="panel">
-	{#each $store_undo.stack as undo_action, i}
-		<p>
-		{#if i === $store_undo.undo_pointer}>{/if}
-		{undo_action.type}: {format_undo_action(undo_action)}
-		</p>
-	{/each}
+  {#each $store_undo.stack as undo_action, i}
+    <p>
+      {#if i === $store_undo.undo_pointer} > {/if}
+      {undo_action.type}: {format_undo_action(undo_action)}
+    </p>
+  {/each}
 </div>
