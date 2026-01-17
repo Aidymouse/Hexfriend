12683b2e64151a7b5167b361e478cd34699c221a
Aidan <aidymouse4891@gmail.com>
2026-01-17 14:37:12 +1300

    Version 3.1 -> 3.2

--- src/components/Settings.svelte
+++ src/components/Settings.svelte
@@ -427,7 +427,7 @@
   <div class="setting-container">
     <h2>{$tl.settings.about.title}</h2>
     <p class="helper-text">
-      Hexfriend v3.1 - {$tl.settings.about.version_tagline}
+      Hexfriend v3.2 - {$tl.settings.about.version_tagline}
     </p>
 
     <p class="helper-text" style="margin-top: var(--small-radius)">
