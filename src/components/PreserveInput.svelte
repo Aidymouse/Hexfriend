8a9b07a5899e72757fde1dc1c682b387407b79c8
Aidan Habedank <aidan.habedank@iotventures.net>
2026-05-13 10:44:36 +1200

    idea for input that preserves its old value

---
+++ src/components/PreserveInput.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte'
+
+  let dispatch = createEventDispatcher()
+
+  export let type: string;
+
+  export let value;
+  let oldValue: string = value
+
+  $: {
+    if (value !== oldValue) {
+      dispatch('change', { new: value, old: oldValue, change_location: 'internal'})
+      oldValue = value
+    } else {
+      dispatch('change', { new: value, old: oldValue, change_location: 'external'})
+      oldValue = value
+    }
+  }
+
+
+</script>
+
+<input type={type} bind:value >
