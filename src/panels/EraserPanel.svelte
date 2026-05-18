e2f6909fe3e39487da4a1b0ffe49cfcc3aa0386c
Aidan <aidymouse4891@gmail.com>
2026-05-18 23:18:19 +1200

    implement undo for eraser panel

--- src/panels/EraserPanel.svelte
+++ src/panels/EraserPanel.svelte
@@ -1,38 +1,45 @@
 <script lang="ts">
+  import { completeUndoState, startUndoState } from "../lib"
     import { store_has_unsaved_changes } from "../stores/flags";
     import { tl } from "../stores/translation";
 
     export let loaded_save;
 
     function erase_all_icons() {
-        if (!confirm($tl.eraser_panel.erase_icons_confirmation)) return;
+        if (!confirm($tl.eraser_panel.erase_icons_confirmation)) { return }
+	startUndoState({icons: loaded_save.icons}, "Erase all icons")
         loaded_save.icons = [];
         $store_has_unsaved_changes = true;
+	completeUndoState({icons: loaded_save.icons})
     }
 
     function erase_all_paths() {
-        if (!confirm($tl.eraser_panel.erase_paths_confirmation)) return;
+        if (!confirm($tl.eraser_panel.erase_paths_confirmation)) { return }
+	startUndoState({paths: loaded_save.paths}, "Erase all paths")
         loaded_save.paths = [];
         $store_has_unsaved_changes = true;
+	completeUndoState({paths: loaded_save.paths})
     }
 
     function erase_all_text() {
-        if (!confirm($tl.eraser_panel.erase_text_confirmation)) return;
+        if (!confirm($tl.eraser_panel.erase_text_confirmation)) { return }
+	startUndoState({texts: loaded_save.texts}, "Erase all texts")
         loaded_save.texts = [];
         $store_has_unsaved_changes = true;
+	completeUndoState({texts: loaded_save.texts})
     }
 </script>
 
 <div class="panel panel-grid">
-    <button class="outline-button" on:click={erase_all_icons}
-        >{$tl.eraser_panel.erase_all_icons}</button
-    >
-    <button class="outline-button" on:click={erase_all_paths}
-        >{$tl.eraser_panel.erase_all_paths}</button
-    >
-    <button class="outline-button" on:click={erase_all_text}
-        >{$tl.eraser_panel.erase_all_text}</button
-    >
+    <button class="outline-button" on:click={erase_all_icons}>
+      {$tl.eraser_panel.erase_all_icons}
+    </button>
+    <button class="outline-button" on:click={erase_all_paths}>
+      {$tl.eraser_panel.erase_all_paths}
+    </button>
+    <button class="outline-button" on:click={erase_all_text}>
+      {$tl.eraser_panel.erase_all_text}
+    </button>
 </div>
 
 <style>
